0cbf473e9eacc0230db46efc78fae8800ae6b786
SolClaude33 <solclaudenft@gmail.com>
2025-09-09 11:42:22 -0300

    Simplify Privy URL parameters to fix connection error

--- js/plugins/PrivyAGWConnect.js
+++ js/plugins/PrivyAGWConnect.js
@@ -71,12 +71,9 @@
             // Get the current origin
             const requesterOrigin = window.location.origin;
             
-            // Create the Privy connection URL - try the original cross-app URL format
-            const privyUrl = `https://privy.abs.xyz/cross-app/connect?` +
-                `connect=true&` +
-                `provider_app_id=cm04asygd041fmry9zmcyn5o5&` +
-                `requester_origin=${encodeURIComponent(requesterOrigin)}&` +
-                `smart_wallet_mode=false&` +
+            // Create the Privy connection URL - try the standard auth URL
+            const privyUrl = `https://auth.privy.io/connect?` +
+                `app_id=cm04asygd041fmry9zmcyn5o5&` +
                 `redirect_uri=${encodeURIComponent(requesterOrigin)}`;
 
             console.log('Opening Privy connection:', privyUrl);
